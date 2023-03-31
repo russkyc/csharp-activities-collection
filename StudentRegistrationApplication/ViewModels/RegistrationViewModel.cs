225145ec156c28f77d842256b11d46c798c77362
russkyc <jrcc1023@gmail.com>
2023-04-01 00:36:57 +0800

    refactor: optimize readability

--- StudentRegistrationApplication/ViewModels/RegistrationViewModel.cs
+++ StudentRegistrationApplication/ViewModels/RegistrationViewModel.cs
@@ -32,26 +32,34 @@ partial class RegistrationViewModel : ObservableObject
 {
     [ObservableProperty]
     private string _firstName;
+    
     [ObservableProperty]
     private string _middleName;
+    
     [ObservableProperty]
     private string _lastName;
+    
     [ObservableProperty]
     private bool _male;
+    
     [ObservableProperty]
     private bool _female;
 
     [ObservableProperty]
     private ObservableCollection<int> _day;
+    
     [ObservableProperty]
     private ObservableCollection<int> _month;
+    
     [ObservableProperty]
     private ObservableCollection<int> _year;
 
     [ObservableProperty]
     private int _selectedDay;
+    
     [ObservableProperty]
     private int _selectedMonth;
+    
     [ObservableProperty]
     private int _selectedYear;
 
@@ -66,20 +74,9 @@ partial class RegistrationViewModel : ObservableObject
         Month = new ObservableCollection<int>();
         Year = new ObservableCollection<int>();
             
-        for (int i = 1; i <= 30; i++)
-        {
-            Day.Add(i);
-        }
-
-        for (int i = 1; i <= 12; i++)
-        {
-            Month.Add(i);
-        }
-
-        for (int i = 1900; i <= 2022; i++)
-        {
-            Year.Add(i);
-        }
+        for (int i = 1; i <= 30; i++) Day.Add(i);
+        for (int i = 1; i <= 12; i++) Month.Add(i);
+        for (int i = 1900; i <= 2022; i++) Year.Add(i);
     }
 
     [RelayCommand]
