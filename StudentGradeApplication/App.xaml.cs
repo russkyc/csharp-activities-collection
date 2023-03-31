7d10fd440c0271f0bd69613e8f0b02fee4f99a32
russkyc <jrcc1023@gmail.com>
2023-04-01 00:24:12 +0800

    chore: update app entry

---
+++ StudentGradeApplication/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using StudentGradeApplication.ViewModels;
+using StudentGradeApplication.Views;
+
+namespace StudentGradeApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            InitializeComponent();
+            base.OnStartup(e);
+
+            var view = new StudentGradeProgramView
+            {
+                DataContext = new StudentGradeProgramViewModel()
+            };
+            
+            view.Show();
+        }
+    }
+}
---
+++ StudentRegistrationApplication/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using StudentRegistrationApplication.ViewModels;
+using StudentRegistrationApplication.Views;
+
+namespace StudentRegistrationApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            InitializeComponent();
+            base.OnStartup(e);
+            
+            var view = new StudentRegistrationView
+            {
+                DataContext = new RegistrationViewModel()
+            };
+            
+            view.Show();
+        }
+    }
+}
